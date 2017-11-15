70e3609916b8a2397bb52d1663e61c1168f04db3
Roland Tritsch <roland@gonito.com>
2017-11-15 15:41:27 +0000

    Fib1: Using OfflineReporter instead of HtmlReporter. Working.

--- src/test/scala/FibonacciBench.scala
+++ src/test/scala/FibonacciBench.scala
@@ -1,35 +1,16 @@
 package org.tritsch.fibonacci
 
 import org.scalameter.api._
-import org.scalameter.picklers.Implicits._
 
-object FibonacciBench extends Bench[Double] {
+object FibonacciBench extends Bench.OfflineReport {
   import Fibonacci._
 
-  // configuration
-  private lazy val defaultRuns = 5
-
-  lazy val executor = SeparateJvmsExecutor(
-    new Executor.Warmer.Default,
-    Aggregator.average,
-    measurer
-  )
-  lazy val measurer = new Measurer.Default
-  lazy val reporter = new HtmlReporter[Double](true)
-  lazy val persistor = Persistor.None
-
   // generators
-
   val ns = Gen.range("n")(0, 29, 1)
 
   // tests
-
   performance of "Fibonacci" in {
-    measure method "fib1" config(
-      exec.minWarmupRuns -> defaultRuns,
-      exec.maxWarmupRuns -> defaultRuns,
-      exec.benchRuns -> defaultRuns
-    ) in {
+    measure method "fib1" in {
       using(ns) in {
         n => fib1(n)
       }
